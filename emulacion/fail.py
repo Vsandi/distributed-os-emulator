0daf3dae05b3a84a63fa001786c7c8393a96fd4a
luis-nav <luiscatodd@gmail.com>
2024-11-22 16:16:01 -0600

    Refactor: Quitar fallos

--- emulacion/fail.py
+++
@@ -1,4 +0,0 @@
-class Fail:
-    def __init__(self, time:float, recovery_time:float):
-        self.time = time
-        self.recovery_time = recovery_time
--- emulacion/job.py
+++ emulacion/job.py
@@ -1,5 +1,4 @@
 from typing import List
-from emulacion.fail import Fail
 from lector_script.instruccion import Instruccion, RecursoInstruccion, FalloInstruccion
 
 class RecursoJob:
@@ -7,12 +6,6 @@ class RecursoJob:
         self.nombre = recurso.nombre
         self.datos = recurso.datos
         self.tiempo = recurso.tiempo
-        self.tiempo_utilizado = 0
-
-class FalloJob:
-    def __init__(self, fallo:FalloInstruccion):
-        self.tiempo = fallo.tiempo
-        self.tiempo_recuperacion = fallo.tiempo_recuperacion
 
 class Job:
     def __init__(self, instruccion: Instruccion):
@@ -20,7 +13,6 @@ class Job:
         self.tiempo = instruccion.tiempo
         self.tiempo_completado = 0
         self.recursos = [RecursoJob(recurso) for recurso in instruccion.recursos]
-        self.recursos = [FalloJob(fallo) for fallo in instruccion.fallos]
     
     def get_tiempo_faltante(self):
         return self.tiempo - self.tiempo_completado
